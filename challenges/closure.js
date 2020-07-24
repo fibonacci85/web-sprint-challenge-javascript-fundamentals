1ccf5a4f4706f9f2fccc45da93d43cdd04bf71aa
Francis Bonacic <francisbonacic@icloud.com>
2020-07-24 10:46:42 -0700

    just one more to go

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,18 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: functions have access to their function scope and if nothing is there they can reach out. 
 
 
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation (param) {   ///////// COME BACK TO THIS!!!!!!!!!!!
+let count = 0;
+return function counter() {
+  return count;
+ }
+}
+
+console.log (summation(4));
